2f054381d38753c33a9a6cd831f5a070c83d3777
Mohdaman01 <mohdastar200026@gmail.com>
2023-06-20 01:03:30 +0530

    added  styling and functionlity to cartitem

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,4 @@
+import './Cart.css'
 import react from 'react';
 import CartItem from './cartItem';
 
@@ -6,15 +7,71 @@ class Cart extends react.Component {
         super();
         
         this.state = {
-            products : ['phone', 'laptop']
+            products : [
+                {
+                    id: 1,
+                    name: 'Laptop',
+                    qty:5
+                },
+                {
+                    id:2,
+                    name: 'Smart Phone',
+                    qty: 10
+                },
+                {
+                    id:3,
+                    name: 'washing machine',
+                    qty:16
+                }
+            ]
         }
+
+    }
+
+    increaseQty =(product)=>{
+         const {products} = this.state;
+         const index = products.indexOf(product);
+
+         products[index].qty += 1;
+         this.setState({
+            products
+         })
+    }
+
+    decreaseQty=(product)=>{
+        if(product.qty === 0){
+            return;
+        }
+
+        const {products} = this.state;
+        const index = products.indexOf(product);
+        products[index].qty -=1;
+
+        this.setState({
+            products
+        })
+    }
+
+    deleteProduct = (id) => {
+        const {products} = this.state;
+
+        const items = products.filter((item)=> item.id !== id);
+
+        this.setState({
+            products: items
+        })
     }
 
     render(){
            return (
                 <div className='cart'>
                     {this.state.products.map((product)=>{
-                        return <CartItem product={product}/>
+                        return <CartItem product={product} 
+                        increaseQty={this.increaseQty} 
+                        decreaseQty={this.decreaseQty}
+                        deleteProduct={this.deleteProduct}
+                        key={product.id}
+                        />
                     })}
                 </div>
             ) 
--- src/cartItem.js
+++ src/cartItem.js
@@ -1,10 +1,26 @@
 import react from 'react';
+import './CartItem.css'
 
 class CartItem extends react.Component {
     render(){
-        return(
+        const product = this.props.product;
+        return( 
+
             <div className='cartItem'>
-                <h2>{this.props.product}</h2>
+                <div className='product-name'>
+                    <h2>{product.name}</h2>
+                </div>
+
+                <div className='product-qty'>
+                    <h2>{product.qty}</h2>
+                </div>
+
+                <div className='icons-container'>
+                    <div onClick={()=>this.props.increaseQty(product)} className='icons'><i className="fa-solid fa-circle-plus"></i></div>
+                    <div className='icons' onClick={()=>{this.props.decreaseQty(product)}}><i className="fa-solid fa-circle-minus"></i></div>
+                    <div className='icons' onClick={()=>{this.props.deleteProduct(product.id)}}><i className="fa-solid fa-trash"></i> </div> 
+                </div>
+                  
             </div> 
         )
     }
